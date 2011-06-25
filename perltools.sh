07d179537c981eb4aa7843aeaeef36e6bd02773e
satzz <satzz21@gmail.com>
2011-06-25 14:54:06 +0900

    modify

--- perltools.sh
+++ perltools.sh
@@ -1,10 +1,11 @@
 # http://webtech-walker.com/archive/2010/04/22173415.html
+mkdir -p ~/bin && cd ~/bin
+
 curl -LO http://xrl.us/perlbrew
 chmod +x perlbrew
 ./perlbrew install
 echo "source $HOME/perl5/perlbrew/etc/bashrc" >> ~/.zshrc
 
-mkdir bin && cd ~/bin
 curl -LO http://xrl.us/cpanm
 chmod +x cpanm
 cpanm local::lib
