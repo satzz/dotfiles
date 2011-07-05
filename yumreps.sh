50d5d85385b400d217186df054cc46f9ab8bd490
satoda.akihiko <root@ip-10-146-14-203.ap-northeast-1.compute.internal>
2011-07-05 01:20:58 -0400

    yum rep

---
+++ yumreps.sh
@@ -0,0 +1,3 @@
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+curl -LO http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el5.rf.i386.rpm
+rpm -i rpmforge-release-0.5.2-2.el5.rf.i386.rpm
