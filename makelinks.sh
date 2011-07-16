3cefdd226b8387e53b112b5693f2e27b4afe99ad
satoda.akihiko <root@ip-10-148-57-106.ap-northeast-1.compute.internal>
2011-07-16 00:09:27 -0400

    ignore warning

--- makelinks.sh
+++ makelinks.sh
@@ -1,1 +1,1 @@
-cd ~; ls -dl ~/config/.* | grep ^\-.* | rev | cut -d' ' -f1 | rev | xargs -I{} ln -s {}
+cd ~; ls -dl ~/config/.* | grep ^\-.* | rev | cut -d' ' -f1 | rev | xargs -I{} ln -s {} 2>/dev/null
